4ef7204b7359b55ddee93ddbb39db7ae8ab8a38b
Rhuan Guilherme <rhuan23032004@gmail.com>
2025-03-25 18:56:59 -0300

    🔧 Configura variáveis de ambiente e valida com Zod

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { app } from "./app";
+import { env } from "./env";
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
+    host: "0.0.0.0",
   })
   .then(() => {
-    console.log(`Server listening at http://localhost:3333`);
+    console.log(`🚀 Server listening at http://localhost:${env.PORT}`);
   });
