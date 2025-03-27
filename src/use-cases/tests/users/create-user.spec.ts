bc25b8bbb287542493252b694f08bd6997705b3d
Rhuan Guilherme <rhuan23032004@gmail.com>
2025-03-27 12:49:33 -0300

    🧪 chore: Configura ambiente de testes com Vitest

---
+++ src/use-cases/tests/users/create-user.spec.ts
@@ -0,0 +1,10 @@
+import { describe, expect, test } from "vitest";
+
+describe("Teste para a criação de un novo usuário.", () => {
+  test("Deve ser possível criar um novo usuário.", () => {
+    // Fase de teste, ainda falta implementar o teste corretamente.
+    const user = true;
+
+    expect(user).toBe(true);
+  });
+});
