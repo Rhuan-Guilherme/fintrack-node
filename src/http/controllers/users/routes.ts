db38a85212426bf50d9760d3dc94ed81d7e0235f
Rhuan Guilherme <rhuan23032004@gmail.com>
2025-04-03 21:18:28 -0300

    ♻️ refactor: Altera nomenclatura da pasta de schemas para padronização

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,13 +1,13 @@
 import { FastifyTypedInstace } from "@/@types/fastify-zod-type";
 import { createUser } from "./create-user";
-import { createUserSchema } from "@/swagger/users/create-user-schema";
+import { createUserSchema } from "@/schemas/users/create-user-schema";
 import { authenticateUser } from "./authenticate";
-import { authenticateSchema } from "@/swagger/users/authenticate-schema";
+import { authenticateSchema } from "@/schemas/users/authenticate-schema";
 import { getUser } from "./get-user";
-import { getUserSchema } from "@/swagger/users/get-user-schema";
+import { getUserSchema } from "@/schemas/users/get-user-schema";
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
 import { refreshToken } from "./refresh-token";
-import { refreshTokenSchema } from "@/swagger/users/refresh-token";
+import { refreshTokenSchema } from "@/schemas/users/refresh-token";
 
 export function userRoutes(app: FastifyTypedInstace) {
   app.post(
